e329aa535520038f5b2190e2b95e9732544874de
Martensit2019 <55350443+Martensit2019@users.noreply.github.com>
2022-03-23 23:33:51 +0300

    Create index.vue

---
+++ index.vue
@@ -0,0 +1,27 @@
+<template>
+<div v-if='!videosIsLoading'>
+    <video-grid :videos="videos" />
+</div>
+	
+</template>
+
+<script>
+import VideoGrid from '~/components/videos/VideoGrid.vue'
+import { defineComponent } from '@nuxtjs/composition-api'
+import { useUser, useData } from '~/lib/compositions'
+export default {
+	name: 'IndexPage',
+	components: { VideoGrid },
+}
+export default defineComponent({
+    setup() {
+        const { user, loggedIn } = useUser()
+        const { data: videos, isLoading: videosIsLoading } = useData('videos.getVideosFeedOnMain')
+        return {
+            user,
+            videos,
+            videosIsLoading
+        }
+    },
+})
+</script>
